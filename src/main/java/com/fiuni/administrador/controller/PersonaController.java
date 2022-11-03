9445a9777afc50210234eeb08415261660e94908
Dahyana <dahybarboza@gmail.com>
2022-11-03 16:13:27 -0300

    Modelo Profesor mejorado

--- src/main/java/com/fiuni/administrador/controller/PersonaController.java
+++ src/main/java/com/fiuni/administrador/controller/PersonaController.java
@@ -2,14 +2,14 @@ package com.fiuni.administrador.controller;
 
 
 import com.fiuni.administrador.dto.persona.PersonaDto;
-import com.fiuni.administrador.dto.rol.RolDto;
+import com.fiuni.administrador.dto.persona.PersonaResult;
 import com.fiuni.administrador.service.persona.IPersonaService;
+import com.fiuni.administrador.utils.Settings;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path ="/personas")
@@ -22,6 +22,30 @@ public class PersonaController {
     public ResponseEntity<PersonaDto> getById(@PathVariable(value = "id") Integer cityId) {
         return personaService.getById(cityId);
     }
+
+    @GetMapping(path = "/page/{page_num}")
+
+    public ResponseEntity<PersonaResult> getClients(@PathVariable(value = "page_num")Integer pageNum){
+        return personaService.getAll(PageRequest.of(pageNum, Settings.PAGE_SIZE));
+
+    }
+
+
+    @PostMapping
+    public ResponseEntity<PersonaDto> save(@Validated @RequestBody PersonaDto persona) {
+        return personaService.save(persona);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PersonaDto> putRol(@PathVariable(value = "id") Integer id, @RequestBody PersonaDto dto) {
+        return personaService.update(id, dto);
+    }
+
+    @DeleteMapping("eliminar/{id}")
+    public ResponseEntity<Boolean> deleteRol(@PathVariable(value = "id") Integer id) {
+        return personaService.delete(id);
+    }
+
 
 
 }
